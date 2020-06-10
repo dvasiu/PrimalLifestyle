414925167d1f94320c02b519d30ef81d3a80757e
dvasiu <dariusvasiu@gmail.com>
2020-06-10 20:13:26 +0200

    added Score, BMI, Calories, Gender to myProfile

--- app/src/main/java/com/example/primallifestyle/MainActivity.kt
+++ app/src/main/java/com/example/primallifestyle/MainActivity.kt
@@ -50,4 +50,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
 }
--- app/src/main/java/com/example/primallifestyle/ui/myprofile/MyProfileFragment.kt
+++ app/src/main/java/com/example/primallifestyle/ui/myprofile/MyProfileFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.primallifestyle.R
+import kotlinx.android.synthetic.main.fragment_myprofile.*
 
 class MyProfileFragment : Fragment() {
 
     private lateinit var myProfileViewModel: MyProfileViewModel
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,10 +26,9 @@ class MyProfileFragment : Fragment() {
         myProfileViewModel =
                 ViewModelProviders.of(this).get(MyProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_myprofile, container, false)
-        val textView: TextView = root.findViewById(R.id.editName)
-        myProfileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+
+
         return root
     }
 }
