410047b5de9409b2d746d6d5b7efeccd3598e241
dvasiu <dariusvasiu@gmail.com>
2020-06-12 12:38:19 +0200

    tested variables in myprofile_fragment

--- app/src/main/java/com/example/primallifestyle/ui/myprofile/MyProfileFragment.kt
+++ app/src/main/java/com/example/primallifestyle/ui/myprofile/MyProfileFragment.kt
@@ -4,42 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.primallifestyle.R
 import com.example.primallifestyle.ui.database.PrimalDatabase
 import com.example.primallifestyle.databinding.FragmentMyprofileBinding
+import com.example.primallifestyle.ui.quiz.QuizFragment
+import kotlinx.android.synthetic.main.fragment_quiz.*
+
 
 class MyProfileFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
+
+
+//        val testingVar: String = "test"
 
         // Get a reference to the binding object and inflate the fragment views.
         val binding: FragmentMyprofileBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_myprofile, container, false)
-
+            inflater, R.layout.fragment_myprofile, container, false
+        )
         val application = requireNotNull(this.activity).application
-
         //Create an instance of the ViewModel Factory
         val dataSource = PrimalDatabase.getInstance(application).primalDatabaseDao
-
         val viewModelFactory = MyProfileViewModelFactory(dataSource, application)
-
         // Get a reference to the ViewModel associated with this fragment
-        val myProfileViewModel = ViewModelProvider(this, viewModelFactory).get(MyProfileViewModel::class.java)
-
+        val myProfileViewModel =
+            ViewModelProvider(this, viewModelFactory).get(MyProfileViewModel::class.java)
         binding.myProfileViewModel = myProfileViewModel
         binding.setLifecycleOwner(this)
+//        binding.nonEditScore.text = testingVar
 
         return binding.root
-
-//        val root = inflater.inflate(R.layout.fragment_myprofile, container, false)
-//return root
     }
 }
 
